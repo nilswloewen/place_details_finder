3e9bcda813cb87cfb75f3a484ae815cf556cdf50
Nils Loewen <nloewen@acromedia.com>
2020-07-01 00:21:18 -0700

    Add write-up for first page.

--- src/taskpane/components/App.js
+++ src/taskpane/components/App.js
@@ -109,41 +109,62 @@ export default class App extends React.Component {
 
     if (!this.state.apiKey || !this.state.validKey) {
       return (
-        <div className="section">
-          <div className="instructions">
-            <span className="bullet">Link with Google Places API</span>
-            Enter your <a href="https://cloud.google.com/maps-platform/">API key</a>.
+          <div className="section">
+            <div className="instructions">
+              <span className="bullet">Link with Google Places API</span>
+              <p>
+                This add-in will enable you to find place data, such as place name, address, phone number, website, latitude, and longitude based on partial address data already in your spreadsheet.
+              </p>
+              <p>
+                This add-in does not require an account or for you to login directly. However, you  will need a Google Account and valid credit card information in order to get your own Google Places API key.
+                Visit <a href="https://cloud.google.com/maps-platform/" target="_blank" rel="noopener noreferrer">Google Maps Platform</a> to create an account.
+              </p>
+              <p>
+                The Places API uses a pay-as-you-go pricing model. Visit <a href="https://developers.google.com/places/web-service/usage-and-billing" target="_blank" rel="noopener noreferrer">Places API Usage and Billing</a> for more information. This add-in does not deal with your payment information in any way.
+              </p>
+              <p>
+                This add-in does not transfer or store your API key. The key is used only in the current session when you are using this add-in.
+              </p>
+              <p>
+                You may need to enter the API key every time you start this add-in. If you have entered an incorrect key, simply close and remove this add-in, and then re-install and open this add-in to enter the correct key.
+              </p>
+              <p>
+                Once you have a Google Places API Key, please enter it here:
+              </p>
+            </div>
+            <label>
+              <input defaultValue={this.state.apiKey} onChange={this.handleChange} placeholder="Paste Key Here" />
+            </label>
           </div>
-          <label>
-            <input defaultValue={this.state.apiKey} onChange={this.handleChange} placeholder="Paste Key Here" />
-          </label>
-        </div>
       );
     }
 
     this.attachSelectionEventToTable();
 
     return (
-      <div>
-        <div id="selected_address" className="hidden" />
-        <div id="selected_row_index" className="hidden" />
-        <div id="rows_selected" className="hidden" />
-        <label>
-          API Key:
-          <div id="apiKey">{this.state.apiKey}</div>
-        </label>
-        <InitOutputRangeBtn />
-        <QueryColumnsTable />
-        <div className="section">
-          <div className="instructions">
-            <span className="bullet">Step 3.</span>
-            Review and modify the query built from your selection.
+        <div>
+          <div id="selected_address" className="hidden" />
+          <div id="selected_row_index" className="hidden" />
+          <div id="rows_selected" className="hidden" />
+          <label>
+            API Key:
+            <div id="apiKey">{this.state.apiKey}</div>
+          </label>
+          <InitOutputRangeBtn />
+          <QueryColumnsTable />
+          <div className="section">
+            <div className="instructions">
+              <span className="bullet">Step 3.</span>
+              Select a row then review and/or modify the query built from your selection.
+              <p>
+                If multiple rows are selected, only the first query will be shown here. When "Search" is clicked, your queries will be sent one at a time automatically.
+              </p>
+            </div>
+            <div contentEditable={true} id="query_input" placeholder={"Click on a row..."} style={{ width: "280px" }} />
           </div>
-          <div contentEditable={true} id="query_input" placeholder={"Click on a row..."} style={{ width: "280px" }} />
+          <GooglePlacesApi apiKey={this.state.apiKey} />
+          <BuildJsonBtn />
         </div>
-        <GooglePlacesApi apiKey={this.state.apiKey} />
-        <BuildJsonBtn />
-      </div>
     );
   }
 }
